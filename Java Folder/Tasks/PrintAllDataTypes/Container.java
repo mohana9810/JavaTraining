2813cb62363ce0641d617b8965a114e23c5f746f
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2023-12-29 11:17:12 +0530

    Add files via upload

---
+++ Java Folder/Tasks/PrintAllDataTypes/Container.java
@@ -0,0 +1,27 @@
+class Container
+{
+public static void main (String[] args)
+{
+String welcomeMessage = "Welcome to Java Class";
+boolean var =true;
+char b ='a';
+int c =15;
+float d =0.5f;
+short e =10;
+long f =20;
+byte g =-8;
+double h= 100.5d;
+
+System.out.println (welcomeMessage);
+System.out.println (var);
+System.out.println (b);
+System.out.println (c);
+System.out.println (d);
+System.out.println (e);
+System.out.println (f);
+System.out.println (g);
+System.out.println (h);
+
+}
+}
+
