19b912b7aa043689538707f88ac1efb278d07a11
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2023-12-29 12:15:26 +0530

    Add files via upload

---
+++ Java Folder/Tasks/PrintAllArrayDataTypes/All.java
@@ -0,0 +1,37 @@
+class All
+{
+public static void main (String[] args)
+{
+int[] age=new int[1];
+age[0]=20;
+byte[] num=new byte[1];
+num[0]=2;
+short[] odd=new short[1];
+odd[0]=3;
+long[] even=new long[1];
+even[0]=30;
+float[] number=new float[1];
+number[0]=0.5f;
+double[] no=new double[1];
+no[0]=100.5d;
+char[] c=new char[1];
+c[0]='c';
+boolean[] var=new boolean[2];
+var[0]=true;
+var[1]=false;
+String[] name=new String[1];
+name[0]="Mohana";
+String[] StringArray = {"apple","mango"};
+System.out.println (age[0]);
+System.out.println (num[0]);
+System.out.println (odd[0]);
+System.out.println (even[0]);
+System.out.println (number[0]);
+System.out.println (no[0]);
+System.out.println (c[0]);
+System.out.println (var[0]);
+System.out.println (var[1]);
+System.out.println (name[0]);
+}
+}
+
