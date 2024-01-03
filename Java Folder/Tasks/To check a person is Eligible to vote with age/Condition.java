4660debb881f9491419381ee7978acc8106a1133
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2024-01-03 21:23:45 +0530

    Add files via upload

---
+++ Java Folder/Tasks/To check a person is Eligible to vote with age/Condition.java
@@ -0,0 +1,16 @@
+class Condition
+{
+public static void main (String[] args)
+{
+int votingAge = 18;
+int inputAge = 21;
+if (inputAge >= votingAge)
+{
+System.out.println ("Eligible");
+}
+else
+{
+System.out.println ("Not Eligible");
+}
+}
+}
