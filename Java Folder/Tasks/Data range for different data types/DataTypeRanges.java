ee0a0c8b334faef88ff79a5c79d778f6431aa3ad
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2024-01-09 18:17:22 +0530

    Add files via upload

---
+++ Java Folder/Tasks/Data range for different data types/DataTypeRanges.java
@@ -0,0 +1,16 @@
+class DataTypeRanges
+{
+public static void main (String[] args)
+{
+System.out.println ("Primitive Data Types:");
+System.out.println ("int - Size: " +Integer .SIZE + "bytes , Range: "+ Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+System.out.println ("long - Size: " +Long .SIZE + "bytes , Range: "+ Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+System.out.println ("float - Size: " +Float .SIZE + "bytes , Range: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+System.out.println ("double - Size: " +Double .SIZE + "bytes , Range: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+System.out.println ("char - Size: " +Character .SIZE + "bytes , Range: " + (int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+System.out.println ("boolean - Size: 1 bit , Range: " + Boolean.FALSE + " to " + Boolean.TRUE);
+System.out.println ("short - Size: " +Short .SIZE + "bytes , Range: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+System.out.println ("byte - Size: " +Byte .SIZE + "bytes , Range: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+}
+}
+
