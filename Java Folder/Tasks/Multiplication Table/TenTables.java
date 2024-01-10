6360965c54e8e760181ca55a49eb048988c2af9d
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2024-01-10 19:30:11 +0530

    Add files via upload

---
+++ Java Folder/Tasks/Multiplication Table/TenTables.java
@@ -0,0 +1,27 @@
+class TenTables
+{
+public static void main (String[] args)
+{
+for (int i=1; i<=10; i++)
+{
+System.out.println ("10*" +i+ "=" +10*i);
+}
+System.out.println ();
+int j=1;
+while (j<=10)
+{
+System.out.println ("3*" +j+ "=" +3*j);
+j++;
+}
+System.out.println ();
+int k=1;
+do
+{
+System.out.println ("4*" +k+ "=" +4*k);
+k++;
+}
+while (k<=10);
+}
+}
+
+
