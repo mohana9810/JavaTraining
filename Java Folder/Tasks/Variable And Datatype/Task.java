fc6201b7714934f823cc3fa83d9ebb1296acd960
mohana9810 <155039976+mohana9810@users.noreply.github.com>
2024-02-17 19:55:14 +0530

    Add files via upload

---
+++ Java Folder/Tasks/Variable And Datatype/Task.java
@@ -0,0 +1,43 @@
+class Task
+{
+public static void main (String[] args)
+{
+int age =25;
+double temparature =98.6;
+boolean isRaining = true;
+char grade ='A';
+String name ="John Doe";
+int num1 =10;
+int num2 =7;
+int num3 =30;
+boolean isEven = (num1 % 2 == 0);
+char initial ='M';
+String city ="Chennai";
+boolean isPositive = (num2 > 0);
+String greeting = "Hello, " + name;
+System.out.println (age);
+System.out.println (temparature);
+System.out.println (isRaining);
+System.out.println(grade);
+System.out.println(name);
+System.out.println(num1);
+System.out.println (num2);
+System.out.println (num3);
+System.out.println (isEven);
+System.out.println(initial);
+System.out.println(city);
+System.out.println(isPositive);
+System.out.println(name);
+System.out.println(greeting);
+}
+}
+
+
+
+
+
+
+
+
+
+
